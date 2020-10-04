66bf3f9bb10c889af09cfacdb79c07120f4f7d4f
devm404 <devinmclaughlin2002@gmail.com>
2020-10-04 14:14:58 -0400

    main

---
+++ README.txt
@@ -0,0 +1,12 @@
+########################################################################################################
+in order to use this you need to install tesseract ocr
+
+https://osdn.net/projects/sfnet_tesseract-ocr-alt/downloads/tesseract-ocr-setup-3.02.02.exe/
+
+make sure the directory in 'main.py' is linked to the location of tesseract.exe
+
+this program will only scan your main monitor so please have the game running on that one
+
+########################################################################################################
+
+published by Devin McLaughlin Oct 1st 2020
---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import numpy as nm
+import pytesseract
+import cv2
+from PIL import ImageGrab
+from playsound import playsound
+
+
+def image_to_string():
+    pytesseract.pytesseract.tesseract_cmd ="C:\Program Files (x86)\Tesseract-OCR\\tesseract.exe"
+
+    while(True):
+
+       cap = ImageGrab.grab(bbox=(700,300,1400,900))
+
+       image_to_text = pytesseract.image_to_string(cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),lang="eng")
+       
+       print(image_to_text)
+
+       if "was The lmpostor" in image_to_text:
+           playsound("wegothim.mp3")
+       
+       if "was An lmpostor" in image_to_text:
+           playsound("wegothim.mp3")
+       
+       if "was An impostor" in image_to_text:
+           playsound("wegothim.mp3")    
+  
+       
+
+image_to_string()
